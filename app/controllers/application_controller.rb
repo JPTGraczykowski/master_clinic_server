928cf9245b5f9b5ee9e1f38a90ec1a963729dd9d
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2022-11-21 00:47:26 +0100

    MCS-02 adjust the application controller to the users and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :telephone])
+  end
 end

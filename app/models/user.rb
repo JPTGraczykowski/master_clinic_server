b15f2fdb38e2021aa3409b82d942f7be9d83f4ae
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2022-11-21 01:20:14 +0100

    MCS-02 configure devise jwt for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, stretches: 13
+         :recoverable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: self, stretches: 13
 
   # == Validations =============================================
   validates :email, :encrypted_password, :first_name, :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_20_231241) do
+ActiveRecord::Schema.define(version: 2022_11_21_001444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2022_11_20_231241) do
     t.boolean "active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
   end
 
 end

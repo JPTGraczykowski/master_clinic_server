1fa7e6cdfcecb141df91122846b6d85583cff3bc
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-03-06 17:31:46 +0100

    MCS-04 allow to edit users and assign cabinets to them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,25 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :telephone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_sign_up_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_account_update_keys)
+  end
+
+  def user_sign_up_keys
+    [
+      :first_name,
+      :last_name,
+      :telephone,
+    ]
+  end
+
+  def user_account_update_keys
+    [
+      :first_name,
+      :last_name,
+      :telephone,
+      :active,
+      :cabinet_id,
+    ]
   end
 end

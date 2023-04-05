ce2c54b97d29eae71d97a479e5b44e72fb717f7b
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-04-05 22:38:40 +0200

    MCS-05 remove no more used devise sign up methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   respond_to :json
 
@@ -30,29 +29,6 @@ class ApplicationController < ActionController::API
       render(json: { message: record.errors.full_messages },
              status: :unprocessable_entity)
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: user_sign_up_keys)
-    devise_parameter_sanitizer.permit(:account_update, keys: user_account_update_keys)
-  end
-
-  def user_sign_up_keys
-    [
-      :first_name,
-      :last_name,
-      :telephone,
-    ]
-  end
-
-  def user_account_update_keys
-    [
-      :first_name,
-      :last_name,
-      :telephone,
-      :active,
-      :cabinet_id,
-    ]
   end
 
   def authenticate_admin!

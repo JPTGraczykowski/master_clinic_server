dacb7a225285ccf6a58bd0643a3852035f25af98
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-04-05 22:26:40 +0200

    MCS-05 add the patients controller

---
+++ app/controllers/patients_controler.rb
@@ -0,0 +1,59 @@
+class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :update, :delete]
+
+  def show
+    render_response(@patient) { true }
+  end
+
+  def new
+    @patient = Patient.new
+
+    render_response(@patient) { true }
+  end
+
+  def create
+    @patient = Patient.new(create_patient_params)
+
+    render_response(@patient, status: :created) { @patient.save }
+  end
+
+  def update
+    @patient.assign_attributes(patient_params)
+
+    render_response(@patient) { @patient.save }
+  end
+
+  def destroy
+    proceed_deletion(@patient)
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def serialize_record
+    PatientSerializer.new(@patient).serialized_json
+  end
+
+  def common_params_keys
+    [
+      :email,
+      :first_name,
+      :last_name,
+      :telephone
+    ]
+  end
+
+  def patient_params
+    params.require(:patient).permit(common_params_keys)
+  end
+
+  def create_patient_params
+    params.require(:patient).permit(
+      *common_params_keys,
+      :password
+    )
+  end
+end
